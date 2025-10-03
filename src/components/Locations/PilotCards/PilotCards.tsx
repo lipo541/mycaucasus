95e9b9b9a146df33ecb2ddddc1d9ce64224a3192
lipo541 <141038060+lipo541@users.noreply.github.com>
2025-10-03 17:00:31 +0400

    x

--- src/components/Locations/PilotCards/PilotCards.tsx
+++ src/components/Locations/PilotCards/PilotCards.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import styles from "./PilotCards.module.css";
 
 interface Pilot {
